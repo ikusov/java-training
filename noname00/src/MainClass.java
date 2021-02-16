01f309be2643a9875472cfc7f3dea5ab524999fc
Danil Ikusov <danil.ikusov@gmail.com>
2021-02-16 22:29:07 +0700

    da third commit

--- noname00/src/MainClass.java
+++ noname00/src/MainClass.java
@@ -1,6 +1,5 @@
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello world! \u263a");
         System.out.println("This is my first java application.");
     }
 }
